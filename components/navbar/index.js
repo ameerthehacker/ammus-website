f5394d4d81f6c76ff6fcb38c9ba6432506d0f136
Ameer Jhan <ameerjhanprof@gmail.com>
2021-03-19 16:07:14 +0530

    animate navbar bg

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Navbar as BNavbar, Nav, NavDropdown } from 'react-bootstrap';
 import { FaBroom, FaInfoCircle } from 'react-icons/fa';
 import { FaQuestionCircle } from 'react-icons/fa';
@@ -5,8 +6,26 @@ import styles from './navbar.module.css';
 
 
 export default function Navbar() {
+  const [bg, setBg] = useState("transparent");
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 30) {
+        setBg("light");
+      } else {
+        setBg("transparent");
+      }
+    };
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, []);
+
   return (
-    <BNavbar variant="dark" fixed="top" expand="lg">
+    <BNavbar bg={bg} variant={bg === "transparent"? "dark": "light"} fixed="top" expand="lg">
       <img height="40px" src="/logo.png" alt="brand-logo"/>
       <BNavbar.Brand className="ml-sm-2" href="#home">Ammus</BNavbar.Brand>
       <BNavbar.Toggle aria-controls="basic-navbar-nav" />
